738efa2203f2e437ceed12ccebc87b4d4b140ec8
Viet Anh Le <mc.vietanh@gmail.com>
2020-04-16 00:07:57 +0700

    updated default

---
+++ HowToBuild.md
@@ -0,0 +1,6 @@
+1. create Architecture
+2. routes
+3. config App
+4. create UI Login
+
+- build depend elements
---
+++ src/routes.js
@@ -0,0 +1,14 @@
+// const routes = [
+//   { path: "/", exact: true, name: "Home" },
+//   { path: "/change-password", name: "Change Passwords", component: ChangePass },
+//   { path: "/dashboard", name: "Dashboard", component: Dashboard },
+//   { path: "/forget-password", name: "Forget Passwords", component: ForgetPass },
+//   { path: "/login-page", name: "Login Page", component: Login },
+//   { path: "/profile-page", name: "Profile Page", component: Profile },
+//   {
+//     path: "reset-passwords",
+//     name: "Reset Passwords Page",
+//     component: ResetPassword,
+//   },
+// ];
+// export default routes;
