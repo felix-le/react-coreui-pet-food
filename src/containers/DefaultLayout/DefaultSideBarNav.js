835e67f5ed531233a4daa2eb299ce248f173b6ed
Viet Anh Le <mc.vietanh@gmail.com>
2020-04-18 17:09:44 +0700

    done render link - need to render title

--- src/containers/DefaultLayout/DefaultSideBarNav.js
+++ src/containers/DefaultLayout/DefaultSideBarNav.js
@@ -1,11 +1,50 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const DefaultSideBarNav = () => {
+  const navItems = [
+    {
+      id: Math.random(),
+      icon: "icon-speedometer",
+      name: "Dashboard",
+      path: "Dashboard",
+      span: "badge badge-info",
+      contenSpan: "NEW",
+    },
+    {
+      id: Math.random(),
+      icon: "icon-drop",
+      name: "Login",
+      path: "login",
+    }
+  ];
+  const NavItem = () => {
+    return(
+      navItems.map((item) => {
+        return(
+          <li className="nav-item" key={item.id}>
+            <Link to={item.path} className="nav-link">
+              <i className="nav-icon `${item.icon}`" />
+              {item.name}
+                {item.span ? 
+                (<span className="{item.span}">{item.contenSpan}</span>) :(null)
+              }
+            </Link>
+          </li>
+        )
+      })
+    )
+  }
+
   return (
-    <>
-      Nav
-    </>
+    <div className="ant-layout-sider">
+      <div className="ant-layout-sider-children">
+        <ul className="nav">
+        <NavItem />
+        </ul>
+      </div>
+    </div>
   );
-}
+};
 
 export default DefaultSideBarNav;
